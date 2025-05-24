55f3124511c320b75ab1430b57adfe7ad81795bb
NoamMichaeli <noam.startech@gmail.com>
2025-05-24 18:14:47 +0300

    fix recommendation

--- backend/algo.py
+++ backend/algo.py
@@ -130,12 +130,12 @@ def get_recommendations_by_user_listening_history(user_id: int):
     if not user_record:
         return []  # user has no likes or dislikes, can't recommend
 
-    top_k_recommendations = 2 * user_record.get('metadata').get('num_tracks')
+    top_k_recommendations = 2 * user_record.metadata.get('num_tracks')
     top_k_recommendations = 50 if top_k_recommendations < 50 else top_k_recommendations
 
     # Query Pinecone 'tracks' index, using 'cosine' metric, to find the top most similar vectors
     query_result = query_pinecone_by_vector('tracks', user_record.values, top_k_recommendations)
-    top_ids_scores = [(match['id'], match['score']) for match in query_result['matches']]
+    top_ids_scores = [(match['id'], match['score']) for match in query_result.matches]
 
     # Get tracks information by 'track_id', and add the similarity 'score' pinecone calculated
     if len(top_ids_scores) > 0:
